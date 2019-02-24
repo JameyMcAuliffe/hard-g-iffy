67344b2e956e68a3e75ba1d89a9eadf2cfc7c3cb
Jamey McAuliffe <jpmcauliffe15@hotmail.com>
2019-02-23 19:23:33 -0600

    copy button copies url to clipboard and toggles with setTimeout

--- src/Giffy/Gifs/GifDetails/GifDetails.js
+++ src/Giffy/Gifs/GifDetails/GifDetails.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 
 import Gif from '../Gif/Gif';
 import { getGifById } from '../../Utils/API';
+import classes from './GifDetails.module.css';
 
 class GifDetails extends Component {
 
 	state = {
 		url: '',
-		alt: ''
+		alt: '',
+		copied: false
 	}
 
 	componentDidMount() {
@@ -15,18 +17,50 @@ class GifDetails extends Component {
 
 		getGifById(id)
 			.then(gif => {
+				console.log('gif: ', gif);
 				let url = gif.data.data.images.original.url;
 				let alt = gif.data.data.title
 				this.setState({url, alt});
 			});
 	}
 
+	copyUrlToClipboard = () => {
+		let el = document.createElement('textarea');
+		el.value = this.state.url;
+		// Set non-editable to avoid focus and move outside of view
+    el.setAttribute('readonly', '');
+    el.style = {position: 'absolute', left: '-9999px'};
+    document.body.appendChild(el);
+    // Select text inside element
+    el.select();
+    // Copy text to clipboard
+    document.execCommand('copy');
+    // Remove temporary element
+    document.body.removeChild(el);
+    //toggle button reflecting url has been copied
+    this.setState({copied: true});
+    setTimeout(() => {
+    	this.setState({copied: false});
+    }, 1000)
+	}
+
 	render() {
 		return (
-			<Gif url={this.state.url} alt={this.state.alt}/>
+			<div className={classes.GifDetails}>
+				<div className={classes.Gif}>
+					<Gif url={this.state.url} alt={this.state.alt}/>
+					<button className={classes.Button}
+									onClick={this.copyUrlToClipboard}>{!this.state.copied ? 'Copy' : 'Copied'}</button><input 
+																			type="text" 
+																			className={classes.Input} 
+																			value={this.state.url} 
+																			readOnly/>
+				</div>
+			</div>
 		);
 	}
 }
 
 export default GifDetails;
+
 
